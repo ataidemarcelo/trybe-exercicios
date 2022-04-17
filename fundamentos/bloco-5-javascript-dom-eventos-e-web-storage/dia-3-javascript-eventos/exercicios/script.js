a1a2cb760a90f541946c7e33b868cd66d2477ce5
Marcelo Ataíde <msa.trybe@gmail.com>
2022-04-17 16:26:16 -0300

    Exercicio-01

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicios/script.js
@@ -0,0 +1,48 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+
+// Exercício 1:
+function addDaysOfTheMonth () {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysContainer = document.querySelector('#days');
+
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    const dayDez = dezDaysList[index];
+    const dayMonth = document.createElement('li');
+    dayMonth.className = 'day';
+
+    const holidayList = [24, 25, 31];
+    for (let holiday of holidayList) {
+      if (dezDaysList[index] === holiday) {
+        dayMonth.classList.add('holiday');
+      }
+    }
+    
+    const fridayList = [4, 11, 18, 25];
+    for (let friday of fridayList) {
+      if (dezDaysList[index] === friday) {
+        dayMonth.classList.add('friday');
+      }
+    }
+
+    dayMonth.innerHTML = dayDez;
+
+    daysContainer.appendChild(dayMonth);
+  }
+}
+
+addDaysOfTheMonth();
