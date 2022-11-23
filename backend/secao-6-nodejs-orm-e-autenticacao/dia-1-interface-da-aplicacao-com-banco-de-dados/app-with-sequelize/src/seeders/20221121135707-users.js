b0f5cc20fdbebccb94bf784c06b0270eec0e1598
Marcelo Ataíde <msa.trybe@gmail.com>
2022-11-23 07:11:51 -0300

    exercicio app-with-sequelize

---
+++ backend/secao-6-nodejs-orm-e-autenticacao/dia-1-interface-da-aplicacao-com-banco-de-dados/app-with-sequelize/src/seeders/20221121135707-users.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [
+      {
+        fullName: 'Leonardo',
+        email: 'leo@email.com',
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      {
+        fullName: 'JEduardo',
+        email: 'edu@email.com',
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    ], {});
+  },
+
+  async down (queryInterface) {
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
